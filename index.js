e326e3f5872f7daae229721c4d9591a6fe3064f5
Jefferson Dantas <jeffersonn.jjdaj@gmail.com>
2020-06-27 16:48:28 -0300

    Pasta models agora se chama modules

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
 const path = require('path')
-const routes = require('./models/routes')
+const routes = require('./modules/routes')
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
